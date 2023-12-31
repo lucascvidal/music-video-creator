b66c122302f6773f7e27c987bc80b20b3cad5076
Lucas Vidal <lcv55@hotmail.com>
2023-12-30 21:37:08 -0300

    Add logic for saving tracks information for later download

--- create_tracks.rb
+++ create_tracks.rb
@@ -22,6 +22,7 @@ end
 
 def create_track(preview_id, bpm, key, template_id, duration, is_premium, template_name, index)
   puts("Creating track with id: #{preview_id}, key: #{key}") if @options[:verbose]
+  track_name = "#{template_name.gsub(" ","-").downcase}_#{index}_#{Time.now.strftime("%Y-%m-%d")}"
   create_track_url = URI("#{ENV['SOUNDFUL_CREATE_TRACK_URL']}/#{preview_id}?mode=create")
   https = Net::HTTP.new(create_track_url.host, create_track_url.port)
   https.use_ssl = true
@@ -32,7 +33,7 @@ def create_track(preview_id, bpm, key, template_id, duration, is_premium, templa
     bpm: bpm,
     duration: duration,
     key: key,
-    name: "#{template_name.gsub(" ","-").downcase}_#{index}_#{Time.now.strftime("%Y-%m-%d")}",
+    name: track_name,
     draft: false,
     template: {
       id: template_id,
@@ -42,7 +43,7 @@ def create_track(preview_id, bpm, key, template_id, duration, is_premium, templa
     
   response = https.request(request)
   puts("Response status: #{response.code}") if @options[:verbose]
-  return JSON.parse(response.read_body)
+  return { response: JSON.parse(response.read_body), track_name: track_name }
 rescue => e
   puts(e)
 end
@@ -61,7 +62,15 @@ def render_track(track_id)
 rescue => e
   puts(e)
 end
-  
+
+def save_track_info(track_info_array)
+  track_info_file = 'tmp/track_info.json'
+
+  File.open(track_info_file, 'w') do |file|
+    file.write(track_info_array.to_json)
+  end
+end
+
 @options = {}
 
 OptionParser.new do |opts|
@@ -69,6 +78,8 @@ OptionParser.new do |opts|
     @options[:verbose] = true
   end
 end.parse!
+
+all_tracks_info = []
 
 KEYS = [
   "f_major",
@@ -104,9 +115,20 @@ KEYS.sample(10).each_with_index do |key, index|
   puts("Waiting a few seconds to render track.") if @options[:verbose]
   sleep(rand(2..7))
 
-  render_track(create_track_response["id"])
+  render_track(create_track_response[:response]["id"])
+  puts("Saving track info for later download.") if @options[:verbose]
+
+  track_info = {
+    'track_id' => create_track_response[:response]["id"],
+    'track_name' => create_track_response[:track_name]
+  }
+  
+  all_tracks_info << track_info
+
   puts("Waiting a few seconds to move on to the next key.") if @options[:verbose]
   sleep(rand(2..7))
 end
+
+save_track_info(all_tracks_info)
 
 puts("Script ran. 10 new tracks should be available at My Library in Soundful if everything went ok.")
