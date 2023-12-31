39adce92f4eeadb0a790ddb641899e653a8b1707
Lucas Vidal <lcv55@hotmail.com>
2023-12-30 21:37:54 -0300

    Rename a few variables and add cleanup commands

--- create_video.sh
+++ create_video.sh
@@ -1,13 +1,20 @@
 #!/bin/zsh
 
-search_dir=tmp/audio
+search_audio_dir=tmp/audio
+search_image_dir=tmp/image
 input_files=()
 
-for entry in "$search_dir"/*
+for audio_entry in "$search_audio_dir"/*
 do
-  input_files+=("-i" "$entry")
+  input_audio_files+=("-i" "$audio_entry")
 done
 
-ffmpeg "${input_files[@]}" -filter_complex "[0:a][1:a][2:a][3:a][4:a][5:a][6:a][7:a][8:a][9:a]concat=n=10:v=0:a=1" tmp/audio_output.mp3
+ffmpeg "${input_audio_files[@]}" -filter_complex "[0:a][1:a][2:a][3:a][4:a][5:a][6:a][7:a][8:a][9:a]concat=n=10:v=0:a=1" tmp/audio_output.mp3
 
-ffmpeg -loop 1 -i tmp/image/input.png -i tmp/audio_output.mp3 -c:v libx264 -c:a aac -strict experimental -b:a 192k -shortest -vf "scale=1920:1080" tmp/video_output.mp4
+ffmpeg -loop 1 -i "$search_image_dir"/input.png -i tmp/audio_output.mp3 -c:v libx264 -c:a aac -strict experimental -b:a 192k -shortest -vf "scale=1920:1080" tmp/video_output.mp4
+
+rm -f "$search_audio_dir"/*
+rm -f "$search_image_dir"/*
+rm tmp/track_info.json
+
+echo "Video created and input files removed."
